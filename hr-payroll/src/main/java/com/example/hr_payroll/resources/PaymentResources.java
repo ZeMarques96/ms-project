96f4b99cd14a321c43d5bedbe9b7b8f447663549
José Francisco Marques Neto <netomarques96@gmail.com>
2025-06-03 12:20:35 -0300

    Resilience4j - CircuitBreaker

--- hr-payroll/src/main/java/com/example/hr_payroll/resources/PaymentResources.java
+++ hr-payroll/src/main/java/com/example/hr_payroll/resources/PaymentResources.java
@@ -2,6 +2,7 @@ package com.example.hr_payroll.resources;
 
 import com.example.hr_payroll.entities.Payment;
 import com.example.hr_payroll.services.PaymentService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,9 +18,17 @@ public class PaymentResources {
     private PaymentService paymentService;
 
     @GetMapping(value = "/{workerId}/days/{days}")
+    @CircuitBreaker(name = "hrWorkerService", fallbackMethod = "getPaymentAlternative")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         Payment payment = paymentService.getPayment(workerId, days);
 
         return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days, Throwable t){
+
+        Payment payment = new Payment("Brann",400.0, days);
+        return ResponseEntity.ok(payment);
+
     }
 }
