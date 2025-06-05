c2003ad88d88a826537173f8fc58ac510b1b0f7c
José Francisco Marques Neto <netomarques96@gmail.com>
2025-06-05 17:02:52 -0300

    Api gateway timeout

--- hr-worker/src/main/java/com/example/hr_worker/entities/Worker.java
+++ hr-worker/src/main/java/com/example/hr_worker/entities/Worker.java
@@ -2,13 +2,8 @@ package com.example.hr_worker.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.IdGeneratorType;
-import org.hibernate.annotations.UuidGenerator;
 
 import java.io.Serializable;
-import java.util.UUID;
-
 
 @Data
 @Entity
--- hr-worker/src/main/java/com/example/hr_worker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/example/hr_worker/resources/WorkerResource.java
@@ -5,7 +5,6 @@ import com.example.hr_worker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +14,6 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
-
-    @Autowired
-    private DiscoveryClient discoveryClient;
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
@@ -37,7 +33,7 @@ public class WorkerResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) throws InterruptedException {
 
-        Thread.sleep(3000L);
+         Thread.sleep(3000L);
 
         logger.info("PORT = " + env.getProperty("local.server.port"));
         Worker worker = workerRepository.findById(id).get();
