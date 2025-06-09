8789e913133d013e69beaf2bb9b1fc615b306c23
José Francisco Marques Neto <netomarques96@gmail.com>
2025-06-09 17:34:10 -0300

    hr-config-server fix

--- hr-config-server/src/main/java/com/example/hr_config_server/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/example/hr_config_server/HrConfigServerApplication.java
@@ -3,6 +3,7 @@ package com.example.hr_config_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class HrConfigServerApplication {
 
